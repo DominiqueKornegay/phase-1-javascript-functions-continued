bdbf1ee46c6fa435f566d896166be04f4e090482
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-05-18 19:57:14 -0400

    commit tree 94c83b11d582c8c65722bcc0f26566400b0d8b56

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(verb) {
+   
+    if (verb) {
+        return `This Saturday, I want to ${verb}!`
+    }
+    else {
+        return `This Saturday, I want to roller-skate!`
+    }
+}
+
+function mondayWork(activity) {
+    if (activity) {
+        return `This Monday, I will ${activity}.`
+    }
+    else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(adj = '*') {
+  return function(name = 'special') {
+      return `You are ${adj}${name}${adj}!`
+  }
+}
